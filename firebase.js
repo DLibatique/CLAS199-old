6e3593b9ebf126782ae6c8e0ccfa53589a94915c
Justin Chiou <chiou.kai@gmail.com>
2018-12-22 20:42:55 -0500

    scaffold firebase into the project

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCjw5zPZM2qF7kd4PUPXND_DIZwi789jb8",
+    authDomain: "doc-upload-8c9d0.firebaseapp.com",
+    databaseURL: "https://doc-upload-8c9d0.firebaseio.com",
+    projectId: "doc-upload-8c9d0",
+    storageBucket: "doc-upload-8c9d0.appspot.com",
+    messagingSenderId: "538953570546"
+};
+
+firebase.initializeApp(config);
